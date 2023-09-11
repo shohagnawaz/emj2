4eb92a38e882d1cc28652910e57c4712b4c9c46d
shohagnawaz <snawazdigital@gmail.com>
2023-09-11 18:59:15 +0600

    cartAdd

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
     const {img, name, price, seller, ratings, quantity} = props.product;
+    const handleAddToCart = props.handleAddToCart;    
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +14,10 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react';
 import './Shop.css';
 import { useEffect } from 'react';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[])
+    },[]);
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'> 
@@ -18,11 +25,14 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
+                <Cart
+                    cart={cart}
+                ></Cart>
             </div>
         </div>
     );
